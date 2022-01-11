654a490bf987b1234ffdba099bcd0176fc67c19e
Gaurav Kumar Sinha <gauravkumarsinhasinha@gmail.com>
2022-01-11 22:43:55 +0530

    Text Util With Toggle Mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import './App.css';
-import About from './Components/About';
+// import About from './Components/About';
 import Navbar from './Components/Navbar'
 import TextForm from './Components/TextForm'
+import React, { useState } from 'react';
+
 
 function App() {
+    const [mode, setMode] = useState('light');
+    const toggleMode = () => {
+        if (mode === 'light'){
+            setMode('dark');
+            document.body.style.background = '#210653';
+            document.body.style.color = 'white';
+        }
+        else{
+            setMode('light');
+            document.body.style.background = 'white';
+            document.body.style.color = 'black';
+        }
+    }
   return (
 <>
-    <Navbar title="TextUtils" aboutText="About TextUtils"/>
+    <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode} />
     <div className="container">
     <TextForm heading="Enter the Text to Analyze!"/>
-    <About />
+    {/* <About /> */}
     </div>
 </>  
   );
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -17,6 +17,18 @@ export default function TextForm(props) {
         let newText = " ";
         setText(newText)
     }
+    const textCopy = ()=>{
+        // console.log("Upper Case was clicked!");
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
+    const removeExtraSpace = ()=>{
+        // console.log("Upper Case was clicked!");
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
     const handleOnChange = (event)=>{
         // console.log("On Change");
         setText(event.target.value);
@@ -25,20 +37,22 @@ export default function TextForm(props) {
     return (
         <>
         <div>
-            <h1>{props.heading}</h1>
+            <h3>{props.heading}</h3>
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" row="8"></textarea>
             </div>
             <button className="btn btn-primary" onClick={handleUpClick}>Convert UpperCase</button>
             <button className="btn btn-primary ms-3" onClick={handleDownClick}>Convert LowerCase</button>
             <button className="btn btn-primary ms-3" onClick={handleClearClick}>Clear</button>
+            <button className="btn btn-primary ms-3" onClick={textCopy}>Copy</button>
+            <button className="btn btn-primary ms-3" onClick={removeExtraSpace}>Remove Spaces</button>
         </div>
         <div className="container my-3">
             <h3>Your Text Summary</h3>
             <p>{text.split(" ").length} words {text.length} characters</p>
             <p>{0.008 * text.split(" ").length} seconds to read this!</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length > 0? text: 'Enter Something Above to Preview Here'}</p>
         </div>
         </>
     )
